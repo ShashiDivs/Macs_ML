d572a5225ffb943b2f0ef4785abf72273e7758bc
shashi <shashipolity@gmail.com>
2024-01-15 10:32:21 +0530

    Model Trainer has been added

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,9 +4,9 @@ import pickle
 import numpy as np
 import pandas as pd
 from src.logger.logging import logging
-from src.exception.exception import customexception
-
+from src.exception.exception import CustomException
 from sklearn.metrics import r2_score, mean_absolute_error,mean_squared_error
+
 
 def save_object(file_path, obj):
     try:
